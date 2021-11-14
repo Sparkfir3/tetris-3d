f881d417bef1e701985352813313f6960de416df
Bill Dang <sparkfire.ffs@gmail.com>
2021-11-14 03:20:02 -0600

    Added proper tetrimino queue display

--- Assets/_Game/Scripts/PlayerController.cs
+++ Assets/_Game/Scripts/PlayerController.cs
@@ -10,6 +10,16 @@ public class PlayerController : MonoBehaviour {
     [SerializeField] private List<GameObject> allTetriminos;
     private List<GameObject> tetriminoStash = new List<GameObject>();
 
+    // ---
+
+    [Header("Queue")]
+    [SerializeField] private Vector3 queuePosition;
+    [SerializeField] private Vector3 queueScale;
+    [SerializeField] private float queueOffset;
+    [SerializeField] private bool displayQueueGizmo;
+
+    // -------------------------------------------------------------------------------
+
     private void Start() {
         FillNextTetriminos();
         grid.OnPlaceTetrimino.AddListener(() => { SpawnNextTetrimino(); });
@@ -29,9 +39,11 @@ public class PlayerController : MonoBehaviour {
     }
 
     private void SpawnNextTetrimino() {
+        nextTetriminos[0].transform.localScale = Vector3.one;
         grid.SetNewTetrimino(nextTetriminos[0]);
         nextTetriminos.RemoveAt(0);
         FillNextTetriminos();
+        RenderQueue();
     }
 
     private void FillNextTetriminos() {
@@ -42,11 +54,31 @@ public class PlayerController : MonoBehaviour {
         // Grab random from stash, spawn it and push to nextTetriminos, then delete from stash
         int index = Random.Range(0, tetriminoStash.Count - 1);
         GameObject newTetrimino = Instantiate(tetriminoStash[index], new Vector3(-20, -20), Quaternion.identity);
+        newTetrimino.transform.localScale = queueScale;
         nextTetriminos.Add(newTetrimino);
         tetriminoStash.RemoveAt(index);
 
         if(nextTetriminos.Count < tetriminoQueueCount)
             FillNextTetriminos();
     }
+
+    private void RenderQueue() {
+        Vector3 nextPosition = queuePosition;
+        for(int i = 0; i < nextTetriminos.Count; i++) {
+            nextTetriminos[i].transform.position = nextPosition;
+            nextPosition += new Vector3(0f, -queueOffset, 0f);
+        }
+    }
+
+    // -------------------------------------------------------------------------------
+
+#if UNITY_EDITOR
+    private void OnDrawGizmos() {
+        if(displayQueueGizmo) {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireCube(queuePosition, Vector3.one);
+        }
+    }
+#endif
 
 }
